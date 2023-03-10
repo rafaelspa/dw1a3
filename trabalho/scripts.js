98c6bdfd89f0d53591cc7bf538f7ae6be597c47e
rafaelspa <rsdepaula67@gmail.com>
2023-03-10 16:24:18 -0300

    refactor addTransaction, small style

--- trabalho/scripts.js
+++ trabalho/scripts.js
@@ -76,12 +76,13 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr);
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
     const amount = Utils.formatCurrency(transaction.amount);
@@ -91,7 +92,7 @@ const DOM = {
         <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
-            <img src="./assets/minus.svg" alt="Remover Transação" />
+            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação" />
         </td>
         `;
 
@@ -205,9 +206,7 @@ const Form = {
 
 const App = {
   init() {
-    Transaction.all.forEach((transaction) => {
-      DOM.addTransaction(transaction);
-    });
+    Transaction.all.forEach(DOM.addTransaction);
 
     DOM.updateBalance();
   },
