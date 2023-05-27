fd584032a38a9df64b52980de58a13ea487112f0
rafaelspa <rsdepaula67@gmail.com>
2023-05-27 17:14:17 -0300

    A10: script.js

--- atividades/A10/formulario_lua_alvaro/js/script.js
+++ atividades/A10/formulario_lua_alvaro/js/script.js
@@ -8,13 +8,29 @@ button.addEventListener('click', (event) => {
 
     if (email.value == '') {
         email.classList.add("errorInput")
+    } else {
+        email.classList.remove("errorInput")
     }
 
     if (password.value == '') {
         password.classList.add("errorInput")
+    } else {
+        password.classList.remove("errorInput")
     }
 
     if (email.value.indexOf("@") == -1 ||  email.value.indexOf(".") || email.value.indexOf("@") - email.value.indexOf(".") == 1) {
         email.classList.add("errorInput")
+    } else {
+        email.classList.remove("errorInput")
+    }
+
+    if (!isNaN(email.value) == true && email.value.length == 11) {
+        email.classList.remove("errorInput")
+    }
+
+    if (password.value.length <= 5) {
+        password.classList.add("errorInput")
+    } else {
+        password.classList.remove("errorInput")
     }
 })
