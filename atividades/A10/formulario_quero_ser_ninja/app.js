c2af640e5ab5c92090eea2dc5c9606598c539dd3
rafaelspa <rsdepaula67@gmail.com>
2023-05-28 11:05:51 -0300

    A10: validação phoneDDI

--- atividades/A10/formulario_quero_ser_ninja/app.js
+++ atividades/A10/formulario_quero_ser_ninja/app.js
@@ -23,6 +23,15 @@ const masks = {
             .replace(/(\d{2})(\d)/, '$1) $2')
             .replace(/(\d{5})(\d)/, '$1-$2')
             .replace(/(-\d{4})./, '$1')
+    },
+    phoneDDI(value) {
+        return value
+            .replace(/\D+/g, '')
+            .replace(/(\d)/, '+$1')
+            .replace(/(\d{2})(\d)/, '$1 ($2')
+            .replace(/(\d{2})(\d)/, '$1) $2')
+            .replace(/(\d{5})(\d)/, '$1-$2')
+            .replace(/(-\d{4})./, '$1')
     }
 };
 
