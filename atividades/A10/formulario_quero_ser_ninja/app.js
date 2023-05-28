18a9baa11f070d63c7af6a8d55fae968791af7c4
rafaelspa <rsdepaula67@gmail.com>
2023-05-28 11:28:22 -0300

    A10: validação pis

--- atividades/A10/formulario_quero_ser_ninja/app.js
+++ atividades/A10/formulario_quero_ser_ninja/app.js
@@ -38,6 +38,14 @@ const masks = {
             .replace(/\D+/g, '')
             .replace(/(\d{5})(\d)/, '$1-$2')
             .replace(/(-\d{3})./, '$1')
+    },
+    pis(value) {
+        return value
+            .replace(/\D+/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{5})(\d)/, '$1.$2')
+            .replace(/(\d{5}).(\d{2})(\d)/, '$1.$2-$3')
+            .replace(/(-\d)./, '$1')
     }
 };
 
