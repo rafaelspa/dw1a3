18f200a50119335c9c6b6e1b2c44f872e94206f4
rafaelspa <rsdepaula67@gmail.com>
2023-05-28 10:16:52 -0300

    A10: validação cnpj

--- atividades/A10/formulario_quero_ser_ninja/app.js
+++ atividades/A10/formulario_quero_ser_ninja/app.js
@@ -1,14 +1,22 @@
 const masks = {
-    cpf (value) {
+    cpf(value) {
       return value
         .replace(/\D+/g, '')
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1-$2')
-        .replace(/(-\d{2}).?/, '$1')
+        .replace(/(-\d{2})./, '$1')
+    },
+    cnpj(value) {
+        return value
+            .replace(/\D+/g, '')
+            .replace(/(\d{2})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1/$2')
+            .replace(/(\d{4})(\d)/, '$1-$2')
+            .replace(/(-\d{2})./, '$1')
     }
 };
-
 
 document.querySelectorAll('input').forEach(($input) => {
     const field = $input.dataset.js
