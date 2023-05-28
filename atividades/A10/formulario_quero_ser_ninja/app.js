a97333d8cf54dc456432c397777a3021c28dc764
rafaelspa <rsdepaula67@gmail.com>
2023-05-28 10:07:12 -0300

    A10: adicionado listeners e validação cpf

---
+++ atividades/A10/formulario_quero_ser_ninja/app.js
@@ -0,0 +1,19 @@
+const masks = {
+    cpf (value) {
+      return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1-$2')
+        .replace(/(-\d{2}).?/, '$1')
+    }
+};
+
+
+document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js
+
+    $input.addEventListener('input', e => {
+        e.target.value = masks[field](e.target.value)
+    }, false)
+})
