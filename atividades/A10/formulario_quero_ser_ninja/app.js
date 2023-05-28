e438875caa9a290886212120c0e65e4a206a9391
rafaelspa <rsdepaula67@gmail.com>
2023-05-28 10:46:42 -0300

    A10: validação telefone

--- atividades/A10/formulario_quero_ser_ninja/app.js
+++ atividades/A10/formulario_quero_ser_ninja/app.js
@@ -15,6 +15,14 @@ const masks = {
             .replace(/(\d{3})(\d)/, '$1/$2')
             .replace(/(\d{4})(\d)/, '$1-$2')
             .replace(/(-\d{2})./, '$1')
+    },
+    phone(value) {
+        return value
+            .replace(/\D+/g, '')
+            .replace(/(\d)/, '($1')
+            .replace(/(\d{2})(\d)/, '$1) $2')
+            .replace(/(\d{5})(\d)/, '$1-$2')
+            .replace(/(-\d{4})./, '$1')
     }
 };
 
