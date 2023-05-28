41d2db1b5e351c1883ed2bb39dcac42760cb383d
rafaelspa <rsdepaula67@gmail.com>
2023-05-28 11:17:05 -0300

    A10: validação cep

--- atividades/A10/formulario_quero_ser_ninja/app.js
+++ atividades/A10/formulario_quero_ser_ninja/app.js
@@ -32,6 +32,12 @@ const masks = {
             .replace(/(\d{2})(\d)/, '$1) $2')
             .replace(/(\d{5})(\d)/, '$1-$2')
             .replace(/(-\d{4})./, '$1')
+    },
+    cep(value) {
+        return value
+            .replace(/\D+/g, '')
+            .replace(/(\d{5})(\d)/, '$1-$2')
+            .replace(/(-\d{3})./, '$1')
     }
 };
 
