50e15a2883f23df32a5fdaaa17d61d4219bade32
rafaelspa <rsdepaula67@gmail.com>
2023-06-01 10:00:39 -0300

    Projeto: api e pokemon

--- projeto/super_trunfo_alura/script.js
+++ projeto/super_trunfo_alura/script.js
@@ -63,6 +63,17 @@ var cartaSuper = {
   },
 };
 
+// const fetchPokemon = async (pokemon) => {
+//   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+//   if (APIResponse.status === 200) {
+//     const data = await APIResponse.json();
+//     return data;
+//   }
+// }
+
+// console.log(fetchPokemon)
+
 var pontosMaquina = 0
 var pontosJogador = 0
 
@@ -233,3 +244,105 @@ function proximaRodada() {
   var divResultado = document.getElementById("resultado")
   divResultado.innerHTML = ""
 }
+
+
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+const numeroDePokemonsComSprite = 649
+var numeroPokemon = parseInt(Math.random() * numeroDePokemonsComSprite);
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+
+const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = 'Carregando...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Não encontrado';
+    pokemonNumber.innerHTML = '';
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+// renderPokemon(numeroPokemon);
+
+
+const exibirPokemonJogador = async (pokemon) => {
+  var divPokemonJogador = document.getElementById("pokemon-jogador");
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    divPokemonJogador.style.backgroundImage = `
+        url(${data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']})
+    `;
+    divPokemonJogador.style.backgroundSize = 'contain';
+    divPokemonJogador.style.backgroundRepeat = 'no-repeat'
+  
+    var nome = `<p class="pokemon-subtitle">${numeroPokemon} - ${data.name}</p>`;
+    opcoesTexto = "";
+
+    for(let i = 0; i < data['stats'].length; i++) {
+      opcoesTexto +=
+        "<input type='radio' name='stat' value='" +
+        data['stats'][i]['stat']['name'] +
+        "' >" +
+        data['stats'][i]['stat']['name'] +
+        ": " +
+        data['stats'][i]['base_stat'] +
+        "</input>"
+        "<br>";
+    }
+
+  var html = "<div id='opcoes' class='pokemon-stats'>";
+
+  divPokemonJogador.innerHTML = nome + html + opcoesTexto + "</div>";
+  } else {
+    alert("Pokemon não encontrado")
+  }
+}
+
+// exibirPokemonJogador(numeroPokemon)
