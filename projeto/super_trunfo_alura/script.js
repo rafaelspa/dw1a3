9c40402e1fa3476c45be3be1979bfb794873bfee
rafaelspa <rsdepaula67@gmail.com>
2023-06-09 10:27:52 -0300

    Projeto: formatacao da imagem dos cards

--- projeto/super_trunfo_alura/script.js
+++ projeto/super_trunfo_alura/script.js
@@ -63,17 +63,6 @@ var cartaSuper = {
   },
 };
 
-// const fetchPokemon = async (pokemon) => {
-//   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
-//   if (APIResponse.status === 200) {
-//     const data = await APIResponse.json();
-//     return data;
-//   }
-// }
-
-// console.log(fetchPokemon)
-
 var pontosMaquina = 0
 var pontosJogador = 0
 
@@ -245,82 +234,12 @@ function proximaRodada() {
   divResultado.innerHTML = ""
 }
 
-const pokemonName = document.querySelector('.pokemon__name');
-const pokemonNumber = document.querySelector('.pokemon__number');
-const pokemonImage = document.querySelector('.pokemon__image');
-
-const form = document.querySelector('.form');
-const input = document.querySelector('.input__search');
-const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('.btn-next');
-
-let searchPokemon = 1;
-
-
-
-const renderPokemon = async (pokemon) => {
-
-  pokemonName.innerHTML = 'Carregando...';
-  pokemonNumber.innerHTML = '';
-
-  const data = await fetchPokemon(pokemon);
-
-  if (data) {
-    pokemonImage.style.display = 'block';
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    input.value = '';
-    searchPokemon = data.id;
-  } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Não encontrado';
-    pokemonNumber.innerHTML = '';
-  }
-}
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  renderPokemon(input.value.toLowerCase());
-});
-
-buttonPrev.addEventListener('click', () => {
-  if (searchPokemon > 1) {
-    searchPokemon -= 1;
-    renderPokemon(searchPokemon);
-  }
-});
-
-buttonNext.addEventListener('click', () => {
-  searchPokemon += 1;
-  renderPokemon(searchPokemon);
-});
-
-// renderPokemon(numeroPokemon);
-
-
-const pokemonJogadorNome = document.querySelector("#pokemon__jogador__nome")
-const pokemonJogadorImagem = document.querySelector("#pokemon__jogador__imagem")
-const pokemonJogadorHp = document.querySelector("#pokemon__jogador__hp")
-const pokemonJogadorAttack = document.querySelector("#pokemon__jogador__attack")
-const pokemonJogadorDefense = document.querySelector("#pokemon__jogador__defense")
-const pokemonJogadorSpecialAttack = document.querySelector("#pokemon__jogador__special__attack")
-const pokemonJogadorSpecialDefense = document.querySelector("#pokemon__jogador__special__defense")
-const pokemonJogadorSpeed = document.querySelector("#pokemon__jogador__speed")
-
-const pokemonMaquinaNome = document.querySelector("#pokemon__maquina__nome")
-const pokemonMaquinaImagem = document.querySelector("#pokemon__maquina__imagem")
-const pokemonMaquinaHp = document.querySelector("#pokemon__maquina__hp")
-const pokemonMaquinaAttack = document.querySelector("#pokemon__maquina__attack")
-const pokemonMaquinaDefense = document.querySelector("#pokemon__maquina__defense")
-const pokemonMaquinaSpecialAttack = document.querySelector("#pokemon__maquina__special__attack")
-const pokemonMaquinaSpecialDefense = document.querySelector("#pokemon__maquina__special__defense")
-const pokemonMaquinaSpeed = document.querySelector("#pokemon__maquina__speed")
-
+// variaveis gerais
 const numeroDePokemonsComSprite = 649 // numero maximo de pokemons com gif
 var numeroPokemonJogador = parseInt(Math.random() * numeroDePokemonsComSprite);
 var numeroPokemonMaquina = parseInt(Math.random() * numeroDePokemonsComSprite);
 
+// metodo para buscar pokemon na api Pokeapi.co
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
@@ -330,39 +249,61 @@ const fetchPokemon = async (pokemon) => {
   }
 }
 
+// variaveis do pokemon do jogador
+const pokemonJogadorNome = document.querySelector("#pokemon__jogador__nome")
+const pokemonJogadorImagem = document.querySelector("#pokemon__jogador__imagem")
+const pokemonJogadorHpValue = document.querySelector("#pokemon__jogador__hp__value")
+const pokemonJogadorAttackValue = document.querySelector("#pokemon__jogador__attack__value")
+const pokemonJogadorDefenseValue = document.querySelector("#pokemon__jogador__defense__value")
+const pokemonJogadorSpecialAttackValue = document.querySelector("#pokemon__jogador__special__attack__value")
+const pokemonJogadorSpecialDefenseValue = document.querySelector("#pokemon__jogador__special__defense__value")
+const pokemonJogadorSpeedValue = document.querySelector("#pokemon__jogador__speed__value")
+
+// metodo para buscar um pokemon para o jogador na api
 const exibirPokemonJogador = async (pokemon) => {
-  pokemonJogadorImagem.src = "./assets/pokeball.jpg"
   const dados = await fetchPokemon(pokemon);
 
   if (dados) {
     pokemonJogadorNome.innerHTML = capitaliza(dados['name'])
     pokemonJogadorImagem.alt = dados.name
     pokemonJogadorImagem.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-    pokemonJogadorHp.innerHTML = `hp: ${dados['stats'][0]["base_stat"]}`
-    pokemonJogadorAttack.innerHTML = `attack: ${dados['stats'][1]["base_stat"]}`
-    pokemonJogadorDefense.innerHTML = `defense: ${dados['stats'][2]["base_stat"]}`
-    pokemonJogadorSpecialAttack.innerHTML = `special-attack: ${dados['stats'][3]["base_stat"]}`
-    pokemonJogadorSpecialDefense.innerHTML = `special-defense: ${dados['stats'][4]["base_stat"]}`
-    pokemonJogadorSpeed.innerHTML = `speed: ${dados['stats'][5]["base_stat"]}`
+    pokemonJogadorHpValue.innerHTML = dados['stats'][0]["base_stat"]
+    pokemonJogadorAttackValue.innerHTML = dados['stats'][1]["base_stat"]
+    pokemonJogadorDefenseValue.innerHTML = dados['stats'][2]["base_stat"]
+    pokemonJogadorSpecialAttackValue.innerHTML = dados['stats'][3]["base_stat"]
+    pokemonJogadorSpecialDefenseValue.innerHTML = dados['stats'][4]["base_stat"]
+    pokemonJogadorSpeedValue.innerHTML = dados['stats'][5]["base_stat"]
 
   } else {
     alert("Pokemon jogador não encontrado")
   }
 }
 
+// variaveis do pokemon da maquina
+const pokemonMaquinaNome = document.querySelector("#pokemon__maquina__nome")
+const pokemonMaquinaImagem = document.querySelector("#pokemon__maquina__imagem")
+const pokemonMaquinaHpValue = document.querySelector("#pokemon__maquina__hp__value")
+const pokemonMaquinaAttackValue = document.querySelector("#pokemon__maquina__attack__value")
+const pokemonMaquinaDefenseValue = document.querySelector("#pokemon__maquina__defense__value")
+const pokemonMaquinaSpecialAttackValue = document.querySelector("#pokemon__maquina__special__attack__value")
+const pokemonMaquinaSpecialDefenseValue = document.querySelector("#pokemon__maquina__special__defense__value")
+const pokemonMaquinaSpeedValue = document.querySelector("#pokemon__maquina__speed__value")
+
+
+// metodo para buscar um pokemon para a maquina na api
 const exibirPokemonMaquina = async (pokemon) => {
   const dados = await fetchPokemon(pokemon);
-
+  
   if (dados) {
     pokemonMaquinaNome.innerHTML = capitaliza(dados['name'])
     pokemonMaquinaImagem.alt = dados.name
     pokemonMaquinaImagem.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-    pokemonMaquinaHp.innerHTML = `hp: ${dados['stats'][0]["base_stat"]}`
-    pokemonMaquinaAttack.innerHTML = `attack: ${dados['stats'][1]["base_stat"]}`
-    pokemonMaquinaDefense.innerHTML = `defense: ${dados['stats'][2]["base_stat"]}`
-    pokemonMaquinaSpecialAttack.innerHTML = `special-attack: ${dados['stats'][3]["base_stat"]}`
-    pokemonMaquinaSpecialDefense.innerHTML = `special-defense: ${dados['stats'][4]["base_stat"]}`
-    pokemonMaquinaSpeed.innerHTML = `speed: ${dados['stats'][5]["base_stat"]}`
+    pokemonMaquinaHpValue.innerHTML = dados['stats'][0]["base_stat"]
+    pokemonMaquinaAttackValue.innerHTML = dados['stats'][1]["base_stat"]
+    pokemonMaquinaDefenseValue.innerHTML = dados['stats'][2]["base_stat"]
+    pokemonMaquinaSpecialAttackValue.innerHTML = dados['stats'][3]["base_stat"]
+    pokemonMaquinaSpecialDefenseValue.innerHTML = dados['stats'][4]["base_stat"]
+    pokemonMaquinaSpeedValue.innerHTML = dados['stats'][5]["base_stat"]
 
   } else {
     alert("Pokemon maquina não encontrado")
