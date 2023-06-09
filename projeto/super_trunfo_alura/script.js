8302c67715a468857e01007bf952a2ca9b6e01cf
rafaelspa <rsdepaula67@gmail.com>
2023-06-09 10:43:57 -0300

    Projeto: limpando o projeto

--- projeto/super_trunfo_alura/script.js
+++ projeto/super_trunfo_alura/script.js
@@ -1,72 +1,5 @@
-var cartaPaulo = {
-  nome: "Shiryu de dragão",
-  imagem:
-    "http://pm1.narvii.com/6399/96fdb9d4fe6a9e72b9bc60ad418e3c43795e53b4_00.jpg",
-  atributos: {
-    ataque: 5,
-    defesa: 9,
-    magia: 10,
-  },
-};
-
-var cartaRafa = {
-  nome: "Bulbasauro",
-  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-  atributos: {
-    ataque: 7,
-    defesa: 8,
-    magia: 6,
-  },
-};
-
-var cartaGui = {
-  nome: "Darth Vader",
-  imagem:
-    "https://images-na.ssl-images-amazon.com/images/I/41i-0NH0q9L._SX328_BO1,204,203,200_.jpg",
-  atributos: {
-    ataque: 9,
-    defesa: 8,
-    magia: 2,
-  },
-};
-
-var cartaBatman = {
-  nome: "Batman",
-  imagem:
-    "https://images3.alphacoders.com/110/1104546.jpg",
-  atributos: {
-    ataque: 7,
-    defesa: 7,
-    magia: 7,
-  },
-};
-
-var cartaLink = {
-  nome: "Link",
-  imagem:
-    "https://www.freepngimg.com/thumb/the_legend_of_zelda/21552-3-zelda-link.png",
-  atributos: {
-    ataque: 5,
-    defesa: 6,
-    magia: 3,
-  },
-};
-
-var cartaSuper = {
-  nome: "Terry Crews",
-  imagem:
-    "https://media.vanityfair.com/photos/5b92cf1758404f52d391c0f0/master/w_768,c_limit/t-Terry-Crews-Interview.jpg",
-  atributos: {
-    ataque: 11,
-    defesa: 11,
-    magia: 11,
-  },
-};
-
 var pontosMaquina = 0
 var pontosJogador = 0
-
-var cartas = [cartaGui, cartaRafa, cartaPaulo, cartaBatman, cartaLink, cartaSuper];
 
 atualizaPlacar()
 atualizaQuantidadeDeCartas()
@@ -87,9 +20,6 @@ function atualizaPlacar() {
   divPlacar.innerHTML = html
 }
 
-var cartaMaquina;
-var cartaJogador;
-
 function sortearCarta() {
   var numeroCartaMaquina = parseInt(Math.random() * cartas.length);
   cartaMaquina = cartas[numeroCartaMaquina];
@@ -103,45 +33,6 @@ function sortearCarta() {
   document.getElementById("btnJogar").disabled = false;
 
   exibirCartaJogador();
-}
-
-function exibirCartaJogador() {
-  var divCartaJogador = document.getElementById("carta-jogador");
-  var moldura =
-    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-' +
-    'transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
-
-  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
-  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
-  opcoesTexto = "";
-
-  for (var atributo in cartaJogador.atributos) {
-    opcoesTexto +=
-      "<input type='radio' name='atributo' value='" +
-      atributo +
-      "' />" +
-      atributo +
-      " " +
-      cartaJogador.atributos[atributo] +
-      "<br>";
-  }
-
-  var html = "<div id='opcoes' class='carta-status'>";
-
-  divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + "</div>";
-}
-
-function exibirOpcoes() {
-  var opcoes = document.getElementById("opcoes");
-  var opcoesTexto = "";
-  for (var atributo in cartaJogador.atributos) {
-    opcoesTexto +=
-      "<input type='radio' name='atributo' value='" +
-      atributo +
-      "' />" +
-      atributo;
-  }
-  opcoes.innerHTML = opcoesTexto;
 }
 
 function obtemAtributoSelecionado() {
@@ -193,32 +84,6 @@ function jogar() {
   atualizaPlacar()
   atualizaQuantidadeDeCartas()
   exibirCartaMaquina();
-}
-
-function exibirCartaMaquina() {
-  var divCartaMaquina = document.getElementById("carta-maquina");
-  var moldura =
-    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-' +
-    'transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
-
-  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
-  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
-  opcoesTexto = "";
-
-  for (var atributo in cartaMaquina.atributos) {
-    opcoesTexto +=
-      "<p type='text' name='atributo' value='" +
-      atributo +
-      "'</p>" +
-      atributo +
-      " " +
-      cartaMaquina.atributos[atributo] +
-      "<br>";
-  }
-
-  var html = "<div id='opcoes' class='carta-status'>";
-
-  divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + "</div>";
 }
 
 function proximaRodada() {
